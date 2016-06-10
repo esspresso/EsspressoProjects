26d83a3b7cd113963fbaf9ce33a9776bf4e47c2d
esspresso <kevin.juma@icloud.com>
2016-06-10 16:30:22 +0300

    Test API Development

---
+++ ExampleAPI/src/com/example/exampleAPI/Example.java
@@ -0,0 +1,14 @@
+package com.example.exampleAPI;
+
+public class Example {
+	
+	public static int addTwoNumbers(){
+		int num1, num2, sum = 0;
+		
+		num1 = 20;
+		num2 = 20;
+		sum = num1 + num2;
+		
+		return sum;
+	}
+}
---
+++ ExampleAPITest/src/Test.java
@@ -0,0 +1,11 @@
+import com.example.exampleAPI.Example;;
+
+public class Test {
+	
+	public static void main(String args[]){
+		Example ex = new Example();
+		System.out.println("Sum is: " + ex.addTwoNumbers());
+		
+	}
+
+}
