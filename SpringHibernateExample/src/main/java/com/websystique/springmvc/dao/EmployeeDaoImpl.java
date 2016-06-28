2ff4fb3212a15b1ea0c5a27bd51371b51fe57f2f
esspresso <kevin.juma@icloud.com>
2016-06-28 13:18:01 +0300

    Integrating Spring with Hibernate using annotation based configuration

---
+++ SpringHibernateExample/src/main/java/com/websystique/springmvc/dao/EmployeeDaoImpl.java
@@ -0,0 +1,45 @@
+package com.websystique.springmvc.dao;
+
+import java.util.List;
+
+import org.hibernate.Criteria;
+import org.hibernate.Query;
+import org.hibernate.criterion.Restrictions;
+import org.springframework.stereotype.Repository;
+
+import com.websystique.springmvc.model.Employee;
+
+@Repository("employeeDao")
+public class EmployeeDaoImpl extends AbstractDao<Integer, Employee> implements EmployeeDao {
+
+    @Override
+	public Employee findById(int id) {
+        return getByKey(id);
+    }
+
+    @Override
+	public void saveEmployee(Employee employee) {
+        persist(employee);
+    }
+
+    @Override
+	public void deleteEmployeeBySsn(String ssn) {
+        Query query = getSession().createSQLQuery("delete from Employee where ssn = :ssn");
+        query.setString("ssn", ssn);
+        query.executeUpdate();
+    }
+
+    @Override
+	@SuppressWarnings({ "unchecked", "cast" })
+    public List<Employee> findAllEmployees() {
+        Criteria criteria = createEntityCriteria();
+        return criteria.list();
+    }
+
+    @Override
+	public Employee findEmployeeBySsn(String ssn) {
+        Criteria criteria = createEntityCriteria();
+        criteria.add(Restrictions.eq("ssn", ssn));
+        return (Employee) criteria.uniqueResult();
+    }
+}
